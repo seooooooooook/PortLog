30c96ee2a9b91f8a26b6712ad7f2b69f83ad7295
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2023-02-25 23:43:59 +0900

    legacy code 정리

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,4 @@
 import bcrypt, { compare } from 'bcrypt';
-import { DBUser } from '../api-conn/user/type';
-import promisePool from '../db-conn/db';
-
-export async function findUserById(id: string): Promise<Array<DBUser>> {
-  const [rows] = await promisePool.query(
-    'select * from user where username= ?;',
-    [id],
-  );
-
-  return rows;
-}
 
 export async function hashPassword(password: string) {
   const hashedPassword = await bcrypt.hash(password, 12);
